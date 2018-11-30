64fbaa920dc679643572465c64d11688ab2fd52d
Barri2k18 <44354611+Barri2k18@users.noreply.github.com>
2018-11-30 20:30:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-
     client.user.setActivity('node.js', {type: 'STREAMING'});
 
 
@@ -27,26 +26,6 @@ client.on('message', msg => {
 
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 // kill me
-    module.exports.run = async (client, message, args) => {
-
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, but you do not have the **Manage Messages** permissions! If you think this is an error, contact an owner.')
-    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send('I do not have the **Manage Messages** permission in this server.');
-
-    if (!args[0]) return message.channel.send('You must specify a number of messages.');
-    if (args[0] < 1) return message.channel.send('Please provide a number greater than 1.');
-    if (args[0] > 100) return message.channel.send('Please provide a number less than 100.');
-    if (isNaN(args[0])) return message.channel.send('Please provide a corect number.');
-
-    message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`🗑 I delete **${args[0]}** messages.`).then(message => message.delete(3000));
-    }).catch().catch((e) => message.channel.send('You can not delete messages older than 14 days.'));
-
-}
-
-module.exports.help = {
-    name: "purge",
-    category: "moderation"
-}
 });
 
 client.login(process.env.TOKEN);
